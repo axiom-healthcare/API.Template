444097b123560c8faed57b0bedd45cb14e5efe63
Flippie <flippie.coetse@gmail.com>
2019-09-29 13:00:36 +0200

    Initial Commit with clean Data Layer

---
+++ Data/Config.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.IO;
+
+namespace Data
+{
+    /// <summary>
+    /// Class used to configure Entity Framework Migrations.
+    /// </summary>
+    class Config
+    {
+        /// <summary>
+        /// Returns the connection string to an SQL Local DB
+        /// </summary>
+        public static string GetConnectionString()
+        {
+            return new DbConnectionStringBuilder
+                {
+                    { "Data Source", @"(localdb)\Data" },
+                    { "AttachDbFilename", Directory.GetCurrentDirectory() + @"\Data\Data.mdf" },
+                    { "Integrated Security", true },
+                    { "database", "Data" }
+                }.ConnectionString;
+        }
+
+        /// <summary>
+        /// Returns the SQL DB configuration
+        /// </summary>
+        public static DbContextOptions<Context> DbOptions()
+        {
+            return new DbContextOptionsBuilder<Context>()
+                .UseSqlServer(Config.GetConnectionString())
+                .Options;
+        }
+    }
+}
---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+
+    /// <summary>
+    /// Class used by Entity Framework Migrations at design time.
+    /// </summary>
+    public class DbFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            return new Context(Config.DbOptions());
+        }
+    }
+}
