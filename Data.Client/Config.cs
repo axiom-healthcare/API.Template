0982f417d61b5fba13909f14a3c0f19231f4e74e
Flippie <Flippie.Coetser@Axiom.Healthcare>
2019-10-13 15:31:48 +0200

    Add Data.Client to CRUD Local SQL DB

---
+++ Data.Client/Config.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Data.Client
+{
+    public class Config
+    {
+        // Connection string to database retrived from appsettings
+        public static string GetConnectionString()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build()
+                .GetConnectionString("Data");
+        }
+
+        public static DbContextOptions<Context> GetDbContextOptions()
+        {
+            return new DbContextOptionsBuilder<Context>()
+                .UseSqlServer(Config.GetConnectionString())
+                .Options;
+        }
+    }
+}
---
+++ Data.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Data.Models;
+using System;
+
+namespace Data.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contextBuilder = Config.GetDbContextOptions();
+
+            using (var data = new Context(contextBuilder))
+            {
+                data.Entities.Add(new Entity()
+                {
+                    Name = "Test"
+                });
+                data.SaveChanges();
+            }
+
+            Console.WriteLine("Entity Created");
+        }
+    }
+}
