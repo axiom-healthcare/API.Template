d0deb57acbc6e617f5dc8e816312e583d398cb9e
Flippie <Flippie.Coetser@Axiom.Healthcare>
2019-10-13 15:29:14 +0200

    Add Test Entity

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Data.Models;
 
 namespace Data
 {
     public class Context: DbContext
     {
         public Context(DbContextOptions<Context> options) : base(options) { }
+
+        // Example DbSet used for illustration purposes
+        public DbSet<Entity> Entities { get; set; }
     }
 
     /// <summary>
