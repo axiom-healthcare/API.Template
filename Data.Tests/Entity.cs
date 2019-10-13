2839e244eaee00ae748e64b70fa4893dd49c177a
Flippie <Flippie.Coetser@Axiom.Healthcare>
2019-10-13 15:29:42 +0200

    Add a Test for Test.Entity

---
+++ Data.Tests/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Data.Tests
+{
+    public class Entity : Base
+    {
+        [Fact]
+        public void ShouldFindOneEntity()
+        {
+            UseSqlite();
+            var data = InitAndGetDbContext();
+
+            Assert.Equal(1, data.Entities.Count(release => release.Name == "Test"));
+
+        }
+        private Context InitAndGetDbContext()
+        {
+            var data = GetDbContext();
+
+            // Use to seed DB before testing
+            data.Entities.Add(new Models.Entity()
+            {
+                Name = "Test"
+            });
+            data.SaveChanges();
+            return data;
+        }
+    }
+}
