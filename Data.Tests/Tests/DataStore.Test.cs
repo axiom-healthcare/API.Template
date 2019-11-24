57ff82e1f663c7f950915be98f2c32ed9c7bed7e
Flippie <flippie.coetse@gmail.com>
2019-11-24 17:41:20 +0100

    Update DataStore Tests to include SqlServer Test

---
+++ Data.Tests/Tests/DataStore.Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Data.Tests
+{
+    public class DataStore : Base
+    {
+        [Fact]
+        public void ShouldBeAbleToUseSqServerDb()
+        {
+            //Arrange
+
+
+            //Act
+            using var data = CreateDbContext(DataStoreType.SqlServer);
+
+            //Assert
+            data.Database.ProviderName.Should().Be("Microsoft.EntityFrameworkCore.SqlServer");
+        }
+        [Fact]
+        public void ShouldBeAbleToUseSqLiteDb()
+        {
+            //Arrange
+
+            //Act
+            var data = CreateDbContext(DataStoreType.Sqlite);
+
+            //Assert
+            data.Database.ProviderName.Should().Be("Microsoft.EntityFrameworkCore.Sqlite");
+        }
+        [Fact]
+        public void ShouldBeAbleToUseInMemoryDb()
+        {
+            //Arrange
+
+            //Act
+            var data = CreateDbContext(DataStoreType.InMemory);
+
+            //Assert
+            data.Database.ProviderName.Should().Be("Microsoft.EntityFrameworkCore.InMemory");
+        } 
+    }
+}
