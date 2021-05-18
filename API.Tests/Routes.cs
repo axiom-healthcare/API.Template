1b1b440e25284143151d75c2c6a745b647e75129
Flippie Coetser <Flippie.Coetser@gmail.com>
2021-05-18 05:17:07 -0700

    Remove redundant code

--- API.Tests/Routes.cs
+++ API.Tests/Routes.cs
@@ -4,10 +4,6 @@
     {
         public const string Root = "api";
 
-        public const string Version = "v1";
-
-        public const string Base = Root + "/" + Version;
-
         public static class Entities
         {
             public static string Create() => Root + "/Entities";
